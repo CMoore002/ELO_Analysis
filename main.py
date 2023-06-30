a01e9ffa525cf5e0cb578d4c6f1513be759c8e93
CMoore002 <cmoore44@myune.edu.au>
2023-06-30 20:57:12 +1000

    exports new csv with ELO data
    main.py now successfully exports a new CSV containing the ELO of the teams before the game is played

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-
 import pandas as pd
 
 def main():
-    eloDict = getNewEloDict()
+    ##Getting input for season to get ELO for
     kValue = 55
     season = input("Enter Season: ")
     fileName = lambda x: "{}_SEASON_NRL_DATA_v1.1".format(x)
-    print(fileName(season))
+
+    ##Reading appropriate text files into pandas dataframe
     dfOne = pd.read_csv(fileName(int(season) - 2))
     dfTwo = pd.read_csv(fileName(int(season) - 1))
     dfThree = pd.read_csv(fileName(season))
-    print(dfOne.iloc[0])
+
+    ##initialising an elo dictionary
+    eloDict = getNewEloDict()
+    eloDictOne, accuracy = processSeasonData(dfOne, kValue, eloDict)
+    eloDictTwo, accuracy = processSeasonData(dfTwo, kValue, eloDictOne)
+
+    ##getting new dataframe and exporting
+    toExport = updateDataframe(dfThree, kValue, eloDictTwo)
+    toExport.to_csv('{}_SEASON_NRL_DATA_v1.2'.format(season))
+
+
 
 
 ##Function that returns new dictionary with default values
@@ -39,11 +49,24 @@ def getNewEloDict():
 
 ##This function processes the seasons data, updating elo values and recording the percentage of accuracy of the ELO model
 ##param df: the dataframe containing the seasons data
-##param kValue: the constant that we're trying to optimise
+##param kValue: a constant
 ##param eloDict: a dictionary containing the ELO ratings of each team
 ##
 def processSeasonData(df, kValue, eloDict):
     correctCount = 0
+
+    ##Calculation of the expected score of team one playing against team two
+    ##param teamOne: ELO rating of team one
+    ##param teamTwo: ELO rating of team two
+    expectedScore = lambda teamOne, teamTwo : 1/(1+10**((teamTwo - teamOne)/400))
+
+    ##Updated ELO calculation
+    ##param oldELO: the teams elo before the match is played
+    ##param kValue: a constant
+    ##param actualScore: the actual outcome. 1 for a win, 0.5 for a tie, 0 for loss
+    ##param expectedScore: the probability of the team winning the match
+    updatedELO = lambda oldELO, kValue, actualScore, expectedScore: oldELO + kValue*(actualScore - expectedScore)
+
     for row in range(len(df)):
         ##getting home team data
         homeTeam = df.loc[row]['Home_Team']
@@ -91,6 +114,62 @@ def splitAxisData(list):
     kValueData = [x[0] for x in list]
     accuracyData = [x[1] for x in list]
     return kValueData, accuracyData
+
+##This function takes a dataframe without ELO and adds ELO for home team and away team
+##param df: the dataframe that is going to be updated with ELO
+##param kValue: a constant
+##param eloDict: dictionary that contains the appropriate elo dictionary required for the season
+def updateDataframe(df, kValue,  eloDict):
+    ##initialsing the header for the new dataframe
+    headers = ['Season', 'Round', 'Venue', 'Kick_Off_Time', 
+               'Home_Team', 'Home_Score', 'Home_ELO', 'Home_Completion', 'Home_Tackle',
+               'Away_Team', 'Away_Score', 'Away_ELO', 'Away_Completion', 'Away_Tackle']
+    newDataframe = pd.DataFrame(columns = headers)
+
+    ##Calculation of the expected score of team one playing against team two
+    ##param teamOne: ELO rating of team one
+    ##param teamTwo: ELO rating of team two
+    expectedScore = lambda teamOne, teamTwo : 1/(1+10**((teamTwo - teamOne)/400))
+
+    ##Updated ELO calculation
+    ##param oldELO: the teams elo before the match is played
+    ##param kValue: a constant
+    ##param actualScore: the actual outcome. 1 for a win, 0.5 for a tie, 0 for loss
+    ##param expectedScore: the probability of the team winning the match
+    updatedELO = lambda oldELO, kValue, actualScore, expectedScore: oldELO + kValue*(actualScore - expectedScore)
+
+    for row in range(len(df)):
+        ##inserting ELO into row of data
+        season, round, venue, kickOffTime = df.loc[row]['Season'], df.loc[row]['Round'], df.loc[row]['Venue'], df.loc[row]['Kick_Off_Time']
+        homeTeam, homeScore, homeCompletion, homeTackle = df.loc[row]['Home_Team'], df.loc[row]['Home_Score'], df.loc[row]['Home_Completion'], df.loc[row]['Home_Tackle']
+        awayTeam, awayScore, awayCompletion, awayTackle = df.loc[row]['Away_Team'], df.loc[row]['Away_Score'], df.loc[row]['Away_Completion'], df.loc[row]['Away_Tackle']
+        homeELO, awayELO = eloDict[homeTeam], eloDict[awayTeam]
+
+        data = [season, round, venue, kickOffTime, homeTeam, homeScore, homeELO, homeCompletion, homeTackle, awayTeam, awayScore, awayELO, awayCompletion, awayTackle]
+        newDataframe.loc[len(newDataframe)] = data
+
+        ##calculating expected outcomes
+        expectedScoreHome = expectedScore(homeELO, awayELO)
+        expectedScoreAway = expectedScore(awayELO, homeELO)
+
+        ##creating actual outcome values
+        if homeScore == awayScore:
+            homeOutcome = 0.5
+            awayOutcome = 0.5
+        elif homeScore > awayScore:
+            homeOutcome = 1
+            awayOutcome = 0
+        else:
+            homeOutcome = 0
+            awayOutcome = 1
+
+        ##calculating and updating ELO values
+        homeNewELO = updatedELO(homeELO, kValue, homeOutcome, expectedScoreHome)
+        awayNewELO = updatedELO(awayELO, kValue, awayOutcome, expectedScoreAway)
+        eloDict[homeTeam] = homeNewELO
+        eloDict[awayTeam] = awayNewELO
+
+    return newDataframe
 
 if __name__ == '__main__':
     main()
